62ef9fd4b2effdfa05083bd16f64cd2a107a49b1
Eric Sabouraud <esabouraud@users.noreply.github.com>
2019-04-24 04:21:23 +0200

    Fix configuration parsing error log

--- discomblebot/confbot.py
+++ discomblebot/confbot.py
@@ -13,7 +13,7 @@ def load_configuration(path):
     config.read(path)
     for mandatory_section in [DISCORD_SECTION, MUMBLE_SECTION]:
         if not config.has_section(mandatory_section):
-            raise ValueError("Configuration file '%s' has no section '%s'")
+            raise ValueError("Configuration file '%s' has no section '%s'" % (path, mandatory_section))
     discord_config = DiscordConf._make([config[DISCORD_SECTION][field] for field in DiscordConf._fields])
     mumble_config = MumbleConf._make([config[MUMBLE_SECTION][field] for field in MumbleConf._fields])
 
